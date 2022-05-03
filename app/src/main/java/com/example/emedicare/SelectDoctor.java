9a4e97cd21ae287d6ae3bfdfb244c0ecb1d6cb1d
ImalshaRC <chaminduimalsha@gmail.com>
2022-05-03 22:50:38 +0530

    latest 1

---
+++ app/src/main/java/com/example/emedicare/SelectDoctor.java
@@ -0,0 +1,14 @@
+package com.example.emedicare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectDoctor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_doctor);
+    }
+}
