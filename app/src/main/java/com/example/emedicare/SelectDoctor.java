f43dcad9750f1c11b329ea09087af3663d5d70ff
Muditha0829 <muditha1gms@gmail.com>
2022-05-04 09:59:33 +0530

    Add Select Doctor content

--- app/src/main/java/com/example/emedicare/SelectDoctor.java
+++ app/src/main/java/com/example/emedicare/SelectDoctor.java
@@ -2,7 +2,10 @@ package com.example.emedicare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SelectDoctor extends AppCompatActivity {
 
@@ -11,4 +14,27 @@ public class SelectDoctor extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_doctor);
     }
+
+    public static void redirectActivity(Activity activity, Class aClass) {
+        Intent intent = new Intent(activity,aClass);
+
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        activity.startActivity(intent);
+
+    }
+
+    public void ClickDoc1(View view){
+        redirectActivity(this, Profile.class);
+    }
+
+    public void ClickDoc2(View view){
+        redirectActivity(this, Profile.class);
+    }
+
+    public void ClickDoc3(View view){
+        redirectActivity(this, Profile.class);
+    }
+
+
 }
